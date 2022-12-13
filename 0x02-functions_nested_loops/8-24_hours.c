8ad0bc67c694d6d4854bacec74d8e330d327599d
Samuel <a2jsamuel@gmail.com>
2022-12-13 16:46:31 +0000

    add 8-24 file

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -17,7 +17,7 @@ void jack_bauer(void)
 	while (i < 51)
 	{
 		j = 48;
-		while (j < 52)
+		while (j < 58)
 		{
 			k = 48;
 			while (k < 54)
@@ -25,6 +25,10 @@ void jack_bauer(void)
 				l = 48;
 				while (l < 58)
 				{
+					if (i >= 50 && j >= 52)
+					{
+						break;
+					}
 					_putchar(i);
 					_putchar(j);
 					_putchar(':');
