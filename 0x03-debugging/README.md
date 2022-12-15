73c54ee7304fbe493baa01a2b9f46bd97689db77
Samuel <a2jsamuel@gmail.com>
2022-12-15 09:39:39 +0000

    add 0-main.c and main.h files

---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+0. The files 0-main.c and main.h testing for source file with positive_or_negative() function
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,8 @@
+#ifndef _main_h_
+#define _main_h_
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+
+#endif
