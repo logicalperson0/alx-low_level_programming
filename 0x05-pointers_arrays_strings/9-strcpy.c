8e5a30b3c6b35b42e2570cd2c8a160811f019599
Samuel <a2jsamuel@gmail.com>
2022-12-19 20:18:41 +0000

    add 9 file

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,9 +1,11 @@
 #include "main.h"
 /**
- * *_strcpy - a fun that copies the str
+ * _strcpy - a fun that copies the str
  *
  * @dest: copy to this parameter
  * @src: the parameter that provides the str
+ *
+ * Return: the copied array of str
  */
 char *_strcpy(char *dest, char *src)
 {
