66293639515f170adaded1465962b965921c3ab7
Samuel <a2jsamuel@gmail.com>
2022-12-22 09:07:39 +0000

    add _strncpy file

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a str
+ *
+ * @dest: the parameter that is copied to
+ * @src: the source to copy from
+ *
+ * Return: the ptr to dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int j;
+
+	for (j = 0; j < n && src[j] != '\0'; j++)
+	{
+		dest[j] = src[j];
+	}
+	for (; j < n; j++)
+	{
+		dest[j] = '\0';
+	}
+	return (dest);
+}
--- 0x06-pointers_arrays_strings/README.md
+++ 0x06-pointers_arrays_strings/README.md
@@ -1,3 +1,5 @@
 0. The file strcat is a function that concatenates two strings.
 
 1. The file strncat is a function that concatenates two strings that use at most n bytes from src
+
+2. The file strncpy is a function that copies a string.
