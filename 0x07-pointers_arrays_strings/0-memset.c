e343d8abb4cab9af1fc208e7e819da163bf1a622
Samuel <a2jsamuel@gmail.com>
2023-01-02 11:58:27 +0000

    add memset file

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _memset - fun that fills memory with constant byte
+ *
+ * @s: a ascii char
+ * @b: a ascii char
+ * @n: a ascuu char
+ *
+ * Return: Always 0 for success
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (0);
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+0. The file _memset is a function that fills memory with a constant byte.
+
