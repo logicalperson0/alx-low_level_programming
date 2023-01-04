fee8479f6d61dacc51cc0c646e6a4b52736b36c3
Samuel <a2jsamuel@gmail.com>
2023-01-04 15:26:37 +0000

    add 4th file

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - function that returns the value of x
+ *  raised to the power of y
+ *
+ *  @x: a ascii char
+ *  @y: a ascii char
+ *
+ *  Return: x^y
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	else if (y == 1)
+		return (x);
+	else
+		return (x *  _pow_recursion(x, (y - 1)));
+}
--- 0x08-recursion/README.md
+++ 0x08-recursion/README.md
@@ -5,3 +5,6 @@
 2. The file strlen_recursion is a function that returns the length of a string.
 
 3. The file factorial is a function that returns the factorial of a given number.
+
+4. Thw file pow_recursion is a function that returns the value of x raised to the power of y
+
