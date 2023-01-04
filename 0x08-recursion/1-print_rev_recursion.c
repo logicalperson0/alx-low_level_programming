d8d917fcb2f3cc506c56938d56d8b0085a2c63fc
Samuel <a2jsamuel@gmail.com>
2023-01-04 14:17:54 +0000

    add 1st file

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - function that prints a string in reverse
+ *
+ * @s: a ascii char
+ *
+ * a void function
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+	}
+	_putchar(*s);
+}
--- 0x08-recursion/README.md
+++ 0x08-recursion/README.md
@@ -1,2 +1,4 @@
 0. The file puts_recursion is a function that prints a string, followed by a new line
 
+1. The file print_rev_recursion is a function that prints a string in reverse.
+
