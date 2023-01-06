3000e29c07a4fc1d655be0d9ea5e0e53be6499d9
Samuel <a2jsamuel@gmail.com>
2023-01-06 13:44:40 +0000

    add 0th file

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - function that returns its name
+ *
+ * Return: name of the fun
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,3 @@
+0. The file whatsmyname is a program that prints its name, followed by a new line
+
+1.
