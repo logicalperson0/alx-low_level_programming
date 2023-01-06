cde639312a745778d80cc4e6b33e01585520ebee
Samuel <a2jsamuel@gmail.com>
2023-01-06 13:45:59 +0000

    add 0th file

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,7 +3,8 @@
 
 /**
  * main - function that returns its name
- *
+ * @argc: parameter that counts num of args on command line
+ * @argv: parameter that is arg vector
  * Return: name of the fun
  */
 int main(int argc, char *argv[])
