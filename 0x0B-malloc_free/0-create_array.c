5d6947b3bdede2891a1f6af4f75ee34af2b1b1b2
Samuel <a2jsamuel@gmail.com>
2023-01-09 13:16:11 +0000

    add 0th file

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,9 +13,13 @@ char *create_array(unsigned int size, char c)
 	char *ptr;
 	unsigned int i;
 
-	ptr = (char *)malloc(size);
 
 	if (size == 0)
+		return (NULL);
+
+	ptr = (char *)malloc(size);
+
+	if (ptr == NULL)
 		return (NULL);
 
 	for (i = 0; i < size; i++)
