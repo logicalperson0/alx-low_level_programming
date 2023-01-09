39b54453cce0eb5d49d33dba1f02cc966507a644
Samuel <a2jsamuel@gmail.com>
2023-01-09 18:07:35 +0000

    add 2nd file

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * str_concat - function that concatenates two strings
+ * @s1: 1st parameter str
+ * @s2: 2nd parameter str
+ * Return: ptr to newly allocated mem in heap
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int i = 0, j = 0, n;
+	char *ptr;
+
+	while (s1[i] != '\0')
+		i++;
+	while (s2[j] != '\0')
+		j++;
+	ptr = (char *)malloc((i + j + 1) * sizeof(char));
+	if (ptr == NULL)
+		return (NULL);
+
+	for (n = 0; n < (i); n++)
+	{
+		ptr[n] = s1[n];
+	}
+	for (n = 0; n < j; n++)
+		ptr[n + i] = s2[n];
+	return (ptr);
+}
--- 0x0B-malloc_free/README.md
+++ 0x0B-malloc_free/README.md
@@ -4,4 +4,4 @@ and initializes it with a specific char
 1. The file _strdup is a function that returns a pointer to a newly allocated space in memory, 
 which contains a copy of the string given as a parameter.
 
-2.
+2. The file str_concat is a function that concatenates two strings
