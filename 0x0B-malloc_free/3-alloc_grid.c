9e9c74350962cfabf7dd987388761364ef84cad4
Samuel <a2jsamuel@gmail.com>
2023-01-10 15:06:22 +0000

    add file

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -16,7 +16,7 @@ int **alloc_grid(int width, int height)
 	if (width < 1 || height < 1)
 		return (NULL);
 
-	ptr = malloc(height * sizeof(int *));
+	ptr = (int **)malloc(height * sizeof(int *));
 	if (ptr == NULL)
 	{
 		free(ptr);
