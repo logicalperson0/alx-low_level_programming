f8760b93ce860bda27ad76802e63ea8993e00d4d
Samuel <a2jsamuel@gmail.com>
2023-01-12 13:09:07 +0000

    add 1st file

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,53 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * string_nconcat - fun that concatenates two strings.
+ * @s1: parameter
+ * @s2: parameter
+ * @n:parameter
+ * Return: ptr which contains s1 followed by n bytes of s2
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *ptr;
+	unsigned int i = 0, j = 0, k;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	if (s1 != NULL)
+	{
+		while (s1[j] != '\0')
+			j++;
+	}
+	if (s2 != NULL)
+	{
+		while (s2[i] != '\0')
+			i++;
+	}
+
+	if (n >= i)
+	{
+		ptr = malloc(sizeof(*ptr) * (i + j + 1));
+		if (ptr == NULL)
+			return (NULL);
+		for (k = 0; k < j; k++)
+			ptr[k] = s1[k];
+		for (k = 0; k < i; k++)
+			ptr[k + j] = s2[k];
+		return (ptr);
+	}
+	else
+	{
+		ptr = malloc(sizeof(*ptr) * (j + n + 1));
+		if (ptr == NULL)
+			return (NULL);
+		for (k = 0; k < j; k++)
+			ptr[k] = s1[k];
+		for (k = 0; k < n; k++)
+			ptr[k + j] = s2[k];
+		return (ptr);
+	}
+}
--- 0x0C-more_malloc_free/README.md
+++ 0x0C-more_malloc_free/README.md
@@ -1,3 +1,5 @@
 0. The file malloc_checked is a function that allocates memory using malloc
 
-1. 
+1. The file string_nconcat is a function that concatenates two strings
+
+2.
