11a7f070527271408991238ea9aa4da809da5c03
Samuel <a2jsamuel@gmail.com>
2023-01-12 15:05:56 +0000

    add 2nd file

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,12 +10,16 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	unsigned int *ptr;
+	char *ptr;
+	unsigned int i;
 
 	if (size == 0 || nmemb == 0)
 		return (NULL);
 	ptr = malloc(size * nmemb);
 	if (ptr == NULL)
 		return (NULL);
+	for (i = 0; i < nmemb; i++)
+		ptr[i] = 0;
+
 	return (ptr);
 }
