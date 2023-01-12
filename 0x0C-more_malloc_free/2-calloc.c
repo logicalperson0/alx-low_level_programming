ec4fe9bb51c283ed00740a42cf5b902c1f23e8e5
Samuel <a2jsamuel@gmail.com>
2023-01-12 14:48:24 +0000

    add 2nd file

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - function that allocates memory for an array, using malloc
+ * @nmemb: parameter array
+ * @size: parameter size of arr
+ * void function
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int *ptr;
+
+	if (size == 0 || nmemb == 0)
+		return (NULL);
+	ptr = malloc(size * nmemb);
+	if (ptr == NULL)
+		return (NULL);
+	return (ptr);
+}
--- 0x0C-more_malloc_free/README.md
+++ 0x0C-more_malloc_free/README.md
@@ -2,4 +2,6 @@
 
 1. The file string_nconcat is a function that concatenates two strings
 
-2.
+2. The file _calloc is a function that allocates memory for an array, using malloc.
+
+3. 
