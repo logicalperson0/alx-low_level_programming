820e3eeb707b3de254c524248e6653f9ee44bebd
Samuel <a2jsamuel@gmail.com>
2023-01-19 12:03:37 +0000

    add 0th file

---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,3 @@
+0. The file sum_them_all is a function that returns the sum of all its parameters.
+
+1. 
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,10 @@
+#ifndef _variadic_functions_h_
+#define _variadic_functions_h_
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+
+int sum_them_all(const unsigned int n, ...);
+
+#endif
