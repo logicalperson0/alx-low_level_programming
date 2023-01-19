53d323e74aadfc70009bc2af1551c1cac82fcc81
Samuel <a2jsamuel@gmail.com>
2023-01-19 12:58:30 +0000

    1st file add

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,34 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - function that prints numbers
+ * @separator: parameter that separates nums
+ * @n: num of args as parameter
+ *
+ * Return: printed nums
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i, j;
+	va_list ap;
+
+	if (separator == NULL)
+		return;
+	if (n == 0)
+		return;
+
+	va_start(ap, n);
+
+	for (i = 0; i < n; i++)
+	{
+		j = (va_arg(ap, int));
+		printf("%d", j);
+
+		if (i == (n - 1))
+			break;
+		printf("%s", separator);
+	}
+	printf("\n");
+
+	va_end(ap);
+}
--- 0x10-variadic_functions/README.md
+++ 0x10-variadic_functions/README.md
@@ -1,3 +1,5 @@
 0. The file sum_them_all is a function that returns the sum of all its parameters.
 
-1. 
+1. The file print_numbers is a function that prints numbers, followed by a new line
+
+2. 
