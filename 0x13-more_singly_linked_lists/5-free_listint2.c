85f0ce14c02fffc7c3a01a19228eb9341efd5d2a
Samuel <a2jsamuel@gmail.com>
2023-01-30 17:16:15 +0000

    add 5th file

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,16 +10,11 @@ void free_listint2(listint_t **head)
 {
 	listint_t *fee;
 
-	if (*head == NULL)
-		printf("(nil)");
-
-	else
+	while (*head !=  NULL)
 	{
-		while (*head !=  NULL)
-		{
-			fee = *head;
-			*head = fee->next;
-			free(fee);
-		}
+		fee = *head;
+		*head = fee->next;
+		free(fee);
 	}
+	*head = NULL;
 }
