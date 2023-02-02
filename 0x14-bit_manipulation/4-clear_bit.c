84078af75c374d4f26d49e098b977a276635836b
Samuel <a2jsamuel@gmail.com>
2023-02-02 17:10:03 +0000

    add 4th file

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * clear_bit - unction that sets the value of a bit to 0 at a given index
+ * @n: parameter
+ * @index: parameter
+ *
+ * Return: 1 for success, otherwise -1
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int tra;
+
+	if (index > 63)
+		return (-1);
+
+	if (*n == 0)
+	{
+		*n = 0;
+		return (1);
+	}
+	else
+	{
+		tra = 1 << index;
+
+		*n = *n ^ tra;
+
+		return (1);
+	}
+}
--- 0x14-bit_manipulation/README.md
+++ 0x14-bit_manipulation/README.md
@@ -6,4 +6,6 @@
 
 3. The file set_bit is a function that sets the value of a bit to 1 at a given index
 
-4. 
+4. The file clear_bit is a function that sets the value of a bit to 0 at a given index
+
+5. 
