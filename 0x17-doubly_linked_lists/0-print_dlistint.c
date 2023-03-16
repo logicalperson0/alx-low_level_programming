e04cc6ead6b67de19f7864578d26115a50c955b2
Samuel <a2jsamuel@gmail.com>
2023-03-16 11:50:01 +0000

    add 0-file

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - function that prints all the elements of a dlistint_t list
+ * @h: parameter for ptr to head of list
+ *
+ * Return: num of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	const dlistint_t *pri;
+	int i = 0;
+
+	pri = malloc(sizeof(dlistint_t));
+	if (pri == NULL)
+		return (0);
+	pri = h;
+
+	while (pri->next != NULL)
+	{
+		printf("%d\n", pri->n);
+		pri = pri->next;
+		i++;
+	}
+	printf("%d\n", pri->n);
+	return (i + 1);
+}
---
+++ 0x17-doubly_linked_lists/README.md
@@ -0,0 +1,3 @@
+0-print_dlistint.c a function that prints all the elements of a dlistint_t list.
+
+
