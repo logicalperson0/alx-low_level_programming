8a082bfe742c29e631a5796ccc1f063a6c307e58
Samuel <a2jsamuel@gmail.com>
2023-07-12 20:27:38 +0300

    This is for time O of jump search of sqrt(n)

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -21,9 +21,6 @@ int jump_search(int *array, size_t size, int value)
 	{
 		printf("Value checked array[%u] = [%d]\n", jump, array[jump]);
 
-		if (array[jump] == value)
-			return (jump);
-
 		jump = (jump + (sqrt(size)));
 
 		if (array[jump] >= value || jump > size)
